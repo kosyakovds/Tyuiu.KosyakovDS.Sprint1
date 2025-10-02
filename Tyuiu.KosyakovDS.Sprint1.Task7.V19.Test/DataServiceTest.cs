10c779a7a64da0deb57b90f6dfd6f1e8d9684a2b
kosyakovds <kosyakov27@mail.ru>
2025-10-02 19:25:30 +0500

    Task7 V19

---
+++ Tyuiu.KosyakovDS.Sprint1.Task7.V19.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KosyakovDS.Sprint1.Task7.V19.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint1.Task7.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(-5.159, res);
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint1.Task7.V19/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KosyakovDS.Sprint1.Task7.V19.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint1.Task7.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Косяков Д. С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Косяков Дмитрий Сергеевич | ИИПб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*     7x^2                                                                *");
+            Console.WriteLine("* x - ---- + sin(x) + |x^4 - x^5| , x = 1                                 *");
+            Console.WriteLine("*     x^3                                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите x:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Вычисления по формуле:" + ds.Calculate(x));
+
+            Console.ReadLine();
+        }
+    }
+}
